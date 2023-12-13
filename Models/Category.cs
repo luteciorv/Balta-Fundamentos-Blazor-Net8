0a7c60477cf8bbddba36eadc51c7f41716fd402a
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-13 15:06:56 -0300

    Mapeamento dos modelos com o DataAnnotation

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,14 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo 'Título' é obrigatório")]
+    [MinLength(3, ErrorMessage = "O campo 'Título' deve conter no mínimo 3 caracteres")]
+    [MaxLength(60, ErrorMessage = "O campo 'Título' deve conter no máximo 60 caracteres")]
     public string Title { get; set; } = string.Empty;
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,21 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Product
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo 'Título' é obrigatório")]
+    [MinLength(3, ErrorMessage = "O campo 'Título' deve conter no mínimo 3 caracteres")]
+    [MaxLength(120, ErrorMessage = "O campo 'Título' deve conter no máximo 120 caracteres")]
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    [Required(ErrorMessage = "Informe o preço do produto")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
 
     public int CategoryId { get; set; }
